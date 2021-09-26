9429a9a6b21735e8b87a52edfc57e837fc843519
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-09-27 00:21:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,130 @@
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 200 && window.screen.width > 1000 ) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+ 
+/*Scroll To Top*/
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/* Swiper Slider - товаров */
+
+  // Собственно, сам хак
+  $(document).on('mouseenter', '.swiper-wrapper .swiper-slide', function() {
+    $(this).closest('.swiper-overflow').addClass("swiper-overflow--hovered");
+  });
+  
+  $(document).on('mouseleave', '.swiper-wrapper .swiper-slide', function() {
+    $(this).closest('.swiper-overflow').removeClass("swiper-overflow--hovered");
+  });
+  
+  // Инициализация слайдера
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      863: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+      1149: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $(document).ready(function() {
+    $('.minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+  });
+
+  // Галерея изображений товара
+
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 5,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".gallery-button-next",
+      prevEl: ".gallery-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+  // Услуги
+
+  $(document).ready(function(){
+    $('ul.contentList li a').click(function(){
+      $('ul.contentList li a').removeClass("active");
+      $(this).addClass("active");
+  });
+  });
