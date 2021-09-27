5095d917c522f78115888875fae8757b7e1d8ada
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-09-27 22:24:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -121,10 +121,15 @@ function backToTop() {
   });
 
   // Услуги
-
-  $(document).ready(function(){
-    $('ul.contentList li a').click(function(){
-      $('ul.contentList li a').removeClass("active");
-      $(this).addClass("active");
-  });
-  });
+  
+  $('.js-tab-trigger').click(function() {
+    var id = $(this).attr('data-tab'),
+        content = $('.js-tab-content[data-tab="'+ id +'"]');
+    
+    $('.js-tab-trigger.active').removeClass('active');
+    $(this).addClass('active');
+    
+    $('.js-tab-content.active').removeClass('active');
+    content.addClass('active');
+ });
+  
